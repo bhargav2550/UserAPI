85f9a52013ddfb6db49ca65e445cc4aeba0e0c3c
Bhargav <106314092+bhargav2550@users.noreply.github.com>
2023-07-14 12:09:32 +0530

    Delete filtersapp directory

--- filtersapp/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from filtersapp.models import users
-
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = users
-        fields = "__all__"
